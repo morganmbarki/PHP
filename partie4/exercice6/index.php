ec755df4bd3147df342fa54453aa7b4fa403c717
unknown <morgan.mbarki@gmail.com>
2019-12-06 09:26:38 +0100

    fin

--- partie4/exercice6/index.php
+++ partie4/exercice6/index.php
@@ -1,4 +1,5 @@
 <?php
+//déclaration d'une variable contenant une valeur numérique
 $numbers = 0;
 ?>
 <!DOCTYPE html>
@@ -11,16 +12,38 @@ $numbers = 0;
     <?php
     while($numbers < 100){
       $numbers = $numbers + 1;
+      //Mettre la condition la plus "complete" en premier (sinon elle passe à la trape)
       if($numbers % 3 == 0 && $numbers % 5 == 0){
     ?>
-    <p><?= 'FizzBuzz'; ?></p>
-    <?php }elseif($numbers % 3 == 0){ ?>
+        <p><?= 'FizzBuzz'; ?></p>
+        <!-- Tout ce qui est deivisé par 3 et dont il reste 0 sera forcement un multiple de 3 -->
+      <?php }elseif($numbers % 3 == 0){ ?>
         <p><?= 'Fizz'; ?></p>
+        <!-- Tout ce qui est deivisé par 5 et dont il reste 0 sera forcement un multiple de 5 -->
       <?php }elseif($numbers % 5 == 0){ ?>
         <p><?= 'Buzz'; ?></p>
       <?php }else{ ?>
+        <!-- Ici ma variable else affichera donc que les nombres qui sont ni multiple de 3 ni de 5 -->
         <p><?= $numbers; ?></p>
-    <?php }
-  } ?>
+    <?php
+      }
+    }
+    ?>
+    <?php
+    //Avec la boucle for
+    for ($i=0; $i <= 100; $i++) {
+      if($i % 3 == 0 && $i % 5 == 0){
+    ?>
+      <p><?= 'FizzBuzz'; ?></p>
+    <?php }elseif($i % 3 == 0){ ?>
+        <p><?= 'Fizz'; ?></p>
+      <?php }elseif($i % 5 == 0){ ?>
+        <p><?= 'Buzz'; ?></p>
+      <?php }else{ ?>
+        <p><?= $i; ?></p>
+  <?php
+      }
+    }
+  ?>
   </body>
 </html>
